c3f0c47a4dc43223d73099f2f726aced5ce0a641
Vivek <vivekdev2411@gmail.com>
2024-05-08 14:51:23 +0530

    sucess

---
+++ javascript/demo.js
@@ -0,0 +1,128 @@
+
+
+// // // // // // // // const arr=[1,2,3,4,5];
+
+// // // // // // // // for(let val of arr){
+// // // // // // // //     console.log(val);
+// // // // // // // // }
+
+// // // // // // // // let list=["hi","hello","bye"];
+
+// // // // // // // // list.forEach(val => console.log(val));
+
+// // // // // // // // let names=["vivek","Maha","Aarthi"];
+
+// // // // // // // // console.log(names.map(val => val+" "+"genious"));
+
+// // // // // // // // let course=["java","sql","mongodb","reactjs"];
+
+// // // // // // // // console.log(course);
+
+// // // // // // // // console.log(course.splice(1,2));
+
+// // // // // // // // console.log(course);
+// // // // // // // // console.log(course);
+// // // // // // // let names=("vivekaarthimahakannan","andra","1233")
+
+// // // // // // // let arr=Array.from(names);
+
+// // // // // // // console.log(arr);
+
+
+// // // // // // let isit=[1,2,3,4,5]
+// // // // // // let arr =Array.isArray(isit);
+// // // // // // console.log(arr);
+
+// // // // // // let a=Array.of("vivek","kannan","maha")
+
+// // // // // // console.log(a);
+
+
+// // // // // let a=[1,2,3,4];
+// // // // // let b=[5,6,7,8];
+// // // // // let c=a.concat(b);
+
+// // // // // console.log(c);
+
+// // // // let arr=[1,2,3,4,5,6,7,8,9];
+
+// // // // let copy=arr.entries();
+
+// // // // console.log(Array.isArray(copy));
+
+// // // function isEven(element, index, array) {
+// // // 	return element % 2 ==0;
+// // // }
+// // // function func() {
+// // // 	let arr = [56, 92, 18, 88, 12];
+
+// // // 	// Check for even number
+// // // 	let value = arr.every(isEven);
+// // // 	console.log(value);
+// // // }
+// // // func();
+
+// // let a=[1,2,3]
+// // let b=[1,2,3]
+
+// // console.log(a.every(b));
+
+
+// // let a=[1,2,3,4,5,6];
+
+// // console.log(a.fill(9,2,4));
+
+// // let array=[1,3,4,5,7,2]
+
+// // let found=array.findIndex(function (element){
+// //     return element % 2==0
+// // });
+
+// // console.log(found)
+
+// let A = [1, 2, 3, 4, 5];
+
+// // Mapping with map method.
+// c = A.map(x => (x * 3));
+// console.log(c);
+
+// let B = [1, 2, 3, 4, 5];
+
+// // Mapping with map method.
+// b = B.map(x => [x,x * 3]);
+// console.log(b);
+
+// function func() {
+// 	let a = [11, 22, 33, 44,55, 66];
+// 	console.log(a.join(''));
+// }
+// func();
+
+// let a=[1,2,3,4,5,3];
+
+// console.log(a.lastIndexOf(3));
+
+// let webDvlop = ["HTML", "CSS", "JS", "Bootstrap"];
+
+// console.log(webDvlop);
+
+// // Add 'React_Native' and 'Php' after removing 'JS'.
+// let removed = webDvlop.splice(-1, 1, 'PHP');
+
+// console.log(webDvlop);
+// console.log(removed);
+
+
+// let str="\'vivek\'";
+
+// console.log(str);
+
+let str1="mind,power,tyre,power";
+
+// let str2=str1.replaceAll("power","rin");
+
+// console.log(str1);
+// console.log(str2);
+
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
